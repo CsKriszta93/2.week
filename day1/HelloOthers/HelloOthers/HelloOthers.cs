779b59a7c1d1800551d3d6c8bf47feb29529909b
Csere Kriszta <kriszta.csere93@gmail.com>
2017-09-11 14:26:58 +0200

    HelloOthers exercise

---
+++ day1/HelloOthers/HelloOthers/HelloOthers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloOthers
+{
+    class HelloOthers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Csilla!");
+            Console.WriteLine("Hello, Botond!");
+            Console.WriteLine("Hello, Tomi!");
+
+            Console.ReadLine();
+
+        }
+    }
+}
