d4af4fa24494aea1e7d33a055a4be5e01da030b8
Csere Kriszta <kriszta.csere93@gmail.com>
2017-09-11 14:53:42 +0200

    IntroduceYourself

---
+++ day1/IntroduceYourself/IntroduceYourself/IntroduceYourself.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroduceYourself
+{
+    class IntroduceYourself
+    {
+        static void Main(string[] args)
+        {
+            string name = "Csere Kriszta";
+            int age = 24;
+            double height = 1,64;
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(height);
+
+            Console.ReadLine();
+
+        }
+    }
+}
