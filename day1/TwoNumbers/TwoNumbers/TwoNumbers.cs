831cd9a97fc70c1f39664460f25550436bae0352
Csere Kriszta <kriszta.csere93@gmail.com>
2017-09-11 14:55:13 +0200

    TwoNumbers exercise

---
+++ day1/TwoNumbers/TwoNumbers/TwoNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoNumbers
+{
+    class TwoNumbers
+    {
+        static void Main(string[] args)
+        {
+            var a = 22;
+            var b = 13;
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(22.0 / 13.0);
+            Console.WriteLine(22 / 13);
+            Console.WriteLine(22 % 13);
+
+            Console.ReadLine();
+        }
+    }
+}
