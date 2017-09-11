4eb06e3923948beb58ab13b0539879167ee1e77e
Csere Kriszta <kriszta.csere93@gmail.com>
2017-09-11 14:02:45 +0200

    HumptyDumpty exercise

---
+++ day1/HumptyDumpty/HumptyDumpty/HumptyDumpty.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumptyDumpty
+{
+    class HumptyDumpty
+    {
+        static void Main(string[] args)
+        {
+            string row1 = ("Humpty Dumpty sat on a wall,");
+            string row2 = ("Humpty Dumpty had a great fall.");
+            string row3 = ("All the king's horses and all the king's men");
+            string row4 = ("Couldn't put Humpty together again.");
+
+            Console.WriteLine(row1 + row2 + row3 + row4);
+
+            Console.ReadLine();
+
+        }
+    }
+}
