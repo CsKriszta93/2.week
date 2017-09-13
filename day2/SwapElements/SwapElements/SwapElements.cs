5055e4119d001ecc592ff502bf518f139558d93b
Csere Kriszta <kriszta.csere93@gmail.com>
2017-09-13 17:01:39 +0200

    finished SwapElements exercise

---
+++ day2/SwapElements/SwapElements/SwapElements.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] abc = { "first", "second", "third" };
+            string swap1 = abc[0];
+            string swap2 = abc[2];
+            abc[2] = swap1;
+            abc[0] = swap2; 
+
+            foreach (string i in abc)
+            {
+                Console.WriteLine(i);
+                Console.ReadLine();
+            }
+
+           
+        }
+    }
+}
