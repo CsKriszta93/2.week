d94021687a6b3986c98763cd4456e27bf91af7f2
Csere Kriszta <kriszta.csere93@gmail.com>
2017-09-13 15:39:07 +0200

    finished DoubleItems

--- day2/DoubleItems/DoubleItems/DoubleItems.cs
+++ day2/DoubleItems/DoubleItems/DoubleItems.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 namespace DoubleItems
 {
-    class Program
+    class DoubleItems
     {
         static void Main(string[] args)
         {
+            int[] ag = { 3, 4, 5, 6, 7 };
+            foreach(int num in ag)
+            {
+                Console.WriteLine(num*2);
+                Console.ReadLine();
+            }
         }
     }
 }
