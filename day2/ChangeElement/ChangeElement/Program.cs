fb6d258b61ffc08a19bf9f8d44de0f0f570b7fa3
Csere Kriszta <kriszta.csere93@gmail.com>
2017-09-13 13:01:34 +0200

    finished exercise ChangeElements

--- day2/ChangeElement/ChangeElement/Program.cs
+++ day2/ChangeElement/ChangeElement/Program.cs
@@ -10,6 +10,28 @@ namespace ChangeElement
     {
         static void Main(string[] args)
         {
+            int[] s = new int[6];
+            s[0] = 1;
+            s[1] = 2;
+            s[2] = 3;
+            s[3] = 8;
+            s[4] = 5;
+            s[5] = 6;
+
+            int fourth = 4;
+            s[3] = fourth;
+
+            Console.WriteLine(s[3]);
+            Console.ReadLine();
+
+            
+
+
+
+
+
+            Console.WriteLine(s[3]);
+            Console.ReadLine();
         }
     }
 }
