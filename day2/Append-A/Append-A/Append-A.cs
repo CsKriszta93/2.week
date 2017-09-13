f82e4d5718d86a450bdde92efe3d51bd684bee74
Csere Kriszta <kriszta.csere93@gmail.com>
2017-09-13 16:46:00 +0200

    Append-A exercise finished

---
+++ day2/Append-A/Append-A/Append-A.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Append_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nimals = { "kuty", "macsk", "cic" };
+
+            foreach (string animal in nimals)
+            {
+                Console.WriteLine(animal + "a");
+                Console.ReadLine();
+            }
+        }
+    }
+}
