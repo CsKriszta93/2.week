02a90574f14a2b9b9a3566cca80efd73e635f5a3
Csere Kriszta <kriszta.csere93@gmail.com>
2017-09-13 14:21:28 +0200

    finished Printall

--- day2/PrintAll/PrintAll/PrintAll.cs
+++ day2/PrintAll/PrintAll/PrintAll.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace PrintAll
 {
-    class Program
+    class PrintAll
     {
         static void Main(string[] args)
         {
+
+            int[] af = { 4, 5, 6, 7 };
+            foreach (int num in af)
+            {
+                Console.WriteLine(num);
+                Console.ReadLine();
+            }
         }
     }
 }
