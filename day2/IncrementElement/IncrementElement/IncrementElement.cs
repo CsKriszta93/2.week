6021c380ee89e234be5fedd0a5829f05b7da5977
Csere Kriszta <kriszta.csere93@gmail.com>
2017-09-13 14:03:07 +0200

    IncrementElement finished

--- day2/IncrementElement/IncrementElement/IncrementElement.cs
+++ day2/IncrementElement/IncrementElement/IncrementElement.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace IncrementElement
 {
-    class Program
+    class IncrementElement
     {
         static void Main(string[] args)
         {
+            int[] t = { 1, 2, 3, 4, 5 };
+            //t[2] = ++t[2];
+            //t[2] += 1; 
+
+           
+            
+            for (int i = 0; i < t.Length; i++)
+            {
+                t[i] += 1;
+                Console.WriteLine(t[i]);
+            }
+
+            /*foreach (int num in t)
+            {
+                Console.WriteLine(num);
+            }*/
+            
+            Console.ReadLine();
+
         }
     }
 }
