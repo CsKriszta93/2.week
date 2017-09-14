0d0a39f21a724c6a21ef2c64705cdedd068a8e13
Csere Kriszta <kriszta.csere93@gmail.com>
2017-09-14 15:42:22 +0200

    TakesLonger exercise quote

---
+++ day3/TakesLonger/TakesLonger/TakesLonger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakesLonger
+{
+    class TakesLonger
+    {
+        static void Main(string[] args)
+        {
+            string quote = "\"Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.\"";
+
+            Console.WriteLine(quote);
+            Console.ReadLine();
+        }
+    }
+}
