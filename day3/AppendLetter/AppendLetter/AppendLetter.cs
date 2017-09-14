bd1620ecf651890649f793902fb31b73cd61adcb
Csere Kriszta <kriszta.csere93@gmail.com>
2017-09-14 22:10:46 +0200

    AppendLetter exercise finished

---
+++ day3/AppendLetter/AppendLetter/AppendLetter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppendLetter
+{
+    class AppendLetter
+    {
+        static void Main(string[] args)
+        {
+            var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+            foreach (string i in far)
+            {
+                Console.WriteLine(i + "a");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
