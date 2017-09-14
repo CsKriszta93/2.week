f89f77e358a61f184f6e660082552841b9da1325
Csere Kriszta <kriszta.csere93@gmail.com>
2017-09-14 11:24:12 +0200

    finished sumall exercise

--- day2/SumAll/SumAll/SumAll.cs
+++ day2/SumAll/SumAll/SumAll.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace SumAll
 {
-    class Program
+    class SumAll
     {
         static void Main(string[] args)
         {
+            int[] ai = { 3, 4, 5, 6, 7 };
+            /*foreach (int number in ai)
+            {
+                Console.WriteLine(number + (number +  1));
+                Console.ReadLine();
+
+            }*/
+
+            int sum = 0;
+
+            for (int i = 0; i < ai.Length; i++)
+            {
+                Console.WriteLine(sum += ai[i]);
+            }
+           
+            Console.ReadLine();
         }
     }
 }
