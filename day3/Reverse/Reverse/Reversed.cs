fba7ef0a486fbe0ff8f5cf77a1e9d7b7257ecb89
Csere Kriszta <kriszta.csere93@gmail.com>
2017-09-14 21:04:24 +0200

    reversed function

---
+++ day3/Reverse/Reverse/Reversed.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse
+{
+    class Reversed
+    {
+        public static string Reversed(string);
+            return (arr)
+
+        static void Main(string[] args)
+        {
+         string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+         char[] arr = reversed.ToCharArray();
+         Console.WriteLine(Reversed());           
+         Console.ReadLine();
+
+
+
+
+
+
+        }
+
+
+    }
+    }
+}
