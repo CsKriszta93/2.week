057ac2c4ccd2fa4632d291f5671b490b3934700f
Csere Kriszta <kriszta.csere93@gmail.com>
2017-09-14 14:35:05 +0200

    01 Replace exercise finished

---
+++ day3/01/01/01.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string example = "In a dishwasher far far away";
+            string example2 = example.Replace("dishwasher", "galaxy");
+
+            Console.WriteLine(example2);
+            Console.ReadLine();
+
+
+        }
+    }
+}
