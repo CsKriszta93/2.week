f2993922a12a1488a561a53f02dfb5640190861b
Csere Kriszta <kriszta.csere93@gmail.com>
2017-09-14 22:05:10 +0200

    Matchmaking exercise

---
+++ day3/Matchmaking/Matchmaking/Matchmaking.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matchmaking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var girls = new List<string> { "Eve", "Ashley", "Bözsi", "Kat", "Jane" };
+            var boys = new List<string> { "Joe", "Fred", "Béla", "Todd", "Neef", "Jeff" };
+
+            var order = new List<string> {};
+            foreach (string i in order)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+           
+
+        }
+    }
+}
