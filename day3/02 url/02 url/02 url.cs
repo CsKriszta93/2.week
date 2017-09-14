056e3ea31c0dc7196d5fec9a874527123b548b53
Csere Kriszta <kriszta.csere93@gmail.com>
2017-09-14 15:30:49 +0200

    02 url exercise finished

---
+++ day3/02 url/02 url/02 url.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_url
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https//www.reddit.com/r/nevertellmethebots";
+            string replace = url.Replace("bots", "odds");
+
+            StringBuilder https = new StringBuilder(replace);
+            https.Insert(5, ':');
+
+            Console.WriteLine(https);
+            Console.ReadLine();
+        }
+    }
+}
