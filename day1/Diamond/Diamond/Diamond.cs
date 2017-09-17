4c8a1c5cbba433db9bc5cc679cb46084d64eb2a8
Csere Kriszta <kriszta.csere93@gmail.com>
2017-09-17 18:22:34 +0200

    half diamond

---
+++ day1/Diamond/Diamond/Diamond.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diamond
+{
+    class Diamond
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int input = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= input; i++)
+            {
+                
+                for (int j = 1; j < input - i + 1; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write('*');
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
