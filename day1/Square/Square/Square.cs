fb51e040aee538d3784f0a1722d25d8879c1f348
Csere Kriszta <kriszta.csere93@gmail.com>
2017-09-17 19:28:30 +0200

    full square

---
+++ day1/Square/Square/Square.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square
+{
+    class Square
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // square like this:
+            //
+            //
+            // %%%%%
+            // %   %
+            // %   %
+            // %   %
+            // %   %
+            // %%%%%
+            //
+            // The square should have as many lines as the number was
+
+            Console.WriteLine("Enter a number: ");
+            int input = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < input; i++)
+            {
+                for (int j = 0; j < input; j++)
+                {
+                    Console.Write("%");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+                /*Console.Write("%");
+                for (int k = 0; k < j - 2; k++)
+                {
+                    Console.Write(" ");
+                    Console.Write("%");
+                }*/
+                
+                
+            
+
+        }
+    }
+}
