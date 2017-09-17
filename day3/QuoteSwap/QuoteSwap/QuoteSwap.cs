342bc6b1100360414cac91d51be6a1b261774c09
Csere Kriszta <kriszta.csere93@gmail.com>
2017-09-17 13:10:17 +0200

    finished QuoteSwap exercise

---
+++ day3/QuoteSwap/QuoteSwap/QuoteSwap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuoteSwap
+{
+    class QuoteSwap
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string> { "What", "I", "do", "create,", "I", "cannot", "not", "understand." };
+            list[2] = "cannot";
+            list[5] = "do";
+            
+            string[] quoteArr = list.ToArray();
+            string wholeQuote = string.Join(" ", quoteArr);
+            Console.WriteLine(wholeQuote);
+            Console.ReadLine();
+
+
+        }
+    }
+}
