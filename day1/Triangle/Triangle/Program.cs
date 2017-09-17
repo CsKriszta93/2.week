3634c6df4789ddaeea5f5a72b59cb368dd97ba9e
Csere Kriszta <kriszta.csere93@gmail.com>
2017-09-17 15:51:16 +0200

    finished Triangle

---
+++ day1/Triangle/Triangle/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int row = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i <= row; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
