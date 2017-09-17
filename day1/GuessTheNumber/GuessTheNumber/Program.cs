38c220bdf3d63ae2cd347d45fb3b722cb21bb4b8
Csere Kriszta <kriszta.csere93@gmail.com>
2017-09-17 20:26:01 +0200

    finsihed Guessthenumber exercise

---
+++ day1/GuessTheNumber/GuessTheNumber/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that stores a number, and the user has to figure it out.
+            // The user can input guesses, after each guess the program would tell one
+            // of the following:
+            //
+            // The stored number is higher
+            // The stried number is lower
+            // You found the number: 8
+
+            int number = 8;
+            Console.WriteLine("I thought of a number. Can you guess it? Enter your guess: ");
+            int input = 0;
+
+            while (input != number)
+            {
+                input = int.Parse(Console.ReadLine());
+                if (input < number)
+                {
+                    Console.WriteLine("The number is higher. Guess again: ");
+                    Console.WriteLine();
+                }
+                else if (input > number)
+                {
+                    Console.WriteLine("The number is lower. Guess again: ");
+                    Console.WriteLine();
+                }
+            }
+            if (input == number)
+            {
+                Console.WriteLine("You guessed it! It was the number 8.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
