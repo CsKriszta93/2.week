5a4c2baa8b87f645e4d26bccee82ed38a6340d2f
Csere Kriszta <kriszta.csere93@gmail.com>
2017-09-17 13:52:37 +0200

    finished PalindromeBuilder

---
+++ day5/PalindromeBuilder/PalindromeBuilder/PalindromeBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in a word");
+            string input = Console.ReadLine();
+            Console.WriteLine(CreatePalindrome(input));
+            Console.ReadLine();
+        }
+
+    static public string CreatePalindrome(string input)
+        {
+            string palindrome = input;
+            
+            for (int i = 0; i < input.Length; i++)
+            {
+                palindrome += input[input.Length - i - 1];
+            }
+            return palindrome;
+            
+        }
+    }
+}
